4a9fd1bc169c92bad76c91fb1dfe40bc9f259a4b
Shreyas9468 <122095667+Shreyas9468@users.noreply.github.com>
2023-02-27 10:40:16 +0530

    Update Login.dart

--- lib/Login.dart
+++ lib/Login.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:teach2/Final.dart';
-import 'package:teach2/Register.dart';
+
+import 'Register.dart';
+
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -13,6 +14,9 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
+    String emailAddress = "";
+    String password = "";
+
     return MaterialApp(
       home: Scaffold(
         body: Center(
@@ -21,17 +25,33 @@ class _LoginState extends State<Login> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               TextField(
-                
+                onChanged: (value){
+                  emailAddress = value;
+                },
                 decoration: InputDecoration(hintText: 'email'),
               ),
               TextField(
-               
+                obscureText: true,
+                onChanged: (value){
+                  password = value;
+                },
                 decoration: InputDecoration(hintText: 'password'),
                 //obscureText: true,
               ),
               ElevatedButton(
-                  onPressed: (){
-                   
+                  onPressed: () async{
+                    try {
+                      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+                          email: emailAddress,
+                          password: password
+                      );
+                    } on FirebaseAuthException catch (e) {
+                      if (e.code == 'user-not-found') {
+                        print('No user found for that email.');
+                      } else if (e.code == 'wrong-password') {
+                        print('Wrong password provided for that user.');
+                      }
+                    }
                   },
                   child: Text('Click to login')),
               Text('Not have an Account'),
